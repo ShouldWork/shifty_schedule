0c004d98399938cadf46cdf144c2b4a5c599aa5a
ShouldWork <mrblocke@gmail.com>
2016-08-22 16:06:02 -0600

    update

--- src/js/nav-bar.component.js
+++ src/js/nav-bar.component.js
@@ -1,6 +1,6 @@
 (function(){
     angular.module("shiftyApp")
         .component('pageHeader', {
-            templateUrl: "src/html/page-header.component.html"
+            templateUrl: "src/html/nav-bar.component.html"
         })
 })();
