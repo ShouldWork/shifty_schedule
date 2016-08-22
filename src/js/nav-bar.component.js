e272559513adf21e861fed17971e95a73696c928
ShouldWork <mrblocke@gmail.com>
2016-08-22 16:48:46 -0600

    update

--- src/js/nav-bar.component.js
+++ src/js/nav-bar.component.js
@@ -1,6 +1,10 @@
 (function(){
     angular.module("shiftyApp")
         .component('navBar', {
-            templateUrl: "src/html/nav-bar.component.html"
+            templateUrl: "src/html/nav-bar.component.html",
+            controller: navController
         })
+        function navController(){
+        	
+        }
 })();
