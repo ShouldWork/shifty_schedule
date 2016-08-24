cd6b7a953bb38e5caea7739abe49bc344dec687c
Krysp <mrblocke@gmail.com>
2016-08-24 09:20:54 -0600

    update

--- src/js/login.component.js
+++ src/js/login.component.js
@@ -1,3 +1,27 @@
-/**
- * Created by Krysp on 8/24/16.
- */
+(function(){
+    angular.module("shiftyApp")
+        .component('login', {
+            templateUrl: "src/html/login.component.html",
+            controller: loginController
+        });
+    function loginController($firebaseObject,$scope,$firebaseArray){
+        var login = this;
+        
+        login.testData = [{text: "Month"},{text: "Year"},{text: "Week"},{text: "Day"},{text: "Level"},{text: "Team"},{text: "Filter"},{text: "Weekends"},{text: "Grave"},{text: "Swing"},{text: "Shift"}];
+        
+        
+        var ref = firebase.database().ref().child("filters");
+        syncObject = $firebaseObject(ref);
+        syncObject.$bindTo($scope,"login.form");
+        syncObject.$loaded().then(function(){
+            login.fireBase = login.form;
+        });
+        
+        
+        login.testSave = function (){
+            console.log("saving...");
+            console.log(JSON.stringify(login.testData));
+            login.form = login.testData;
+        };
+    }
+})();
