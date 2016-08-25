04f1b1729ff8cb29d73b9c6be02176b542aed718
ShouldWork <mrblocke@gmail.com>
2016-08-25 15:14:51 -0600

    update

--- src/js/login.component.js
+++ src/js/login.component.js
@@ -8,7 +8,7 @@
         var login = this;
         
         login.testData = [{text: "Month"},{text: "Year"},{text: "Week"},{text: "Day"},{text: "Level"},{text: "Team"},{text: "Filter"},{text: "Weekends"},{text: "Grave"},{text: "Swing"},{text: "Shift"}];
-        
+        login.login = login; 
         
         var ref = firebase.database().ref().child("filters");
         syncObject = $firebaseObject(ref);
@@ -23,5 +23,39 @@
             console.log(JSON.stringify(login.testData));
             login.form = login.testData;
         };
+
+
+        function login(provider) {
+            var auth = $firebaseAuth();
+            // login with provider
+            auth.$signInWithPopup(provider).then(function (firebaseUser) {
+                $log.log(firebaseUser);
+                vm.displayName = firebaseUser.user.displayName;
+                vm.providerUser = firebaseUser.user;
+                var ref = firebase.database().ref("users");
+                var profileRef = ref.child(vm.providerUser.uid);
+                vm.user = $firebaseObject(profileRef);
+                $log.log(vm.user);
+                $log.log(profileRef);
+                vm.user.$loaded().then(function () {
+                    if (!vm.user.displayName) {
+                        $log.log("creating user...");
+                        profileRef.set({
+                            displayName: vm.providerUser.displayName,
+                            email: vm.providerUser.email,
+                            photoURL: vm.providerUser.photoURL
+                        }).then(function () {
+                            $log.log("user created.");
+                        }, function () {
+                            $log.log("user could not be created.");
+                        });
+                    } else {
+                        $log.log('user already created!');
+                    }
+                });
+            }).catch(function (error) {
+                $log.log("Authentication failed:", error);
+            });
+        }
     }
 })();
