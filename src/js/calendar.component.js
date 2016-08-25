a97ce264b6d8727fcbad3117e513a597c2fa2eae
Krysp <mrblocke@gmail.com>
2016-08-25 09:05:09 -0600

    update

--- src/js/calendar.component.js
+++ src/js/calendar.component.js
@@ -6,6 +6,11 @@
         });
     function calendarController($firebaseObject,$scope,$firebaseArray){
         var cal = this;
+
+
+        cal.shifts = [{text: "6:00 am - 3:00 pm"},{text: "7:00 am - 4:00 pm"},{text: "8:00 am - 5:00 pm"},{text: "9:00 am - 6:00 pm"},{text: "10:00 am - 7:00 pm"},{text: "11:00 am - 8:00 pm"},{text: "2:00 pm - 10:00 pm"}];
+        
+        
         // var ref = firebase.database().ref().child("filters");
         // syncObject = $firebaseObject(ref);
         // syncObject.$bindTo($scope,"login.form");
