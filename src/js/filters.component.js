29a7d56278da0e658fae024109aac54fca355057
ShouldWork <mrblocke@gmail.com>
2016-08-26 20:25:41 -0600

    update

--- src/js/filters.component.js
+++ src/js/filters.component.js
@@ -37,7 +37,7 @@
 			ref.child(id).once("value", function(snapshot) {
 				var data = snapshot.val();
 				var toggle = (data.set) ? false : true;
-				(!toggle) ? filter.showToast("Removing " + data.name + " from filters",1000) : filter.showToast("Adding " + data.name + " to filters",1000);	
+				(!toggle) ? filter.showToast("Removing '" + data.name + "' from filters",1000) : filter.showToast("Adding '" + data.name + "' to filters",1000);	
 				ref.child(id).child("set").set(toggle);
 				// data === "hello"
 			});
