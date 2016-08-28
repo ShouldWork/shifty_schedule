cd461769ec85ec494c608be672c36c63795ecb7c
ShouldWork <mrblocke@gmail.com>
2016-08-27 20:12:11 -0600

    update

--- src/services/schedule.service.js
+++ src/services/schedule.service.js
@@ -124,7 +124,7 @@
                     self.displayList = getList("xactware/techs",false);
                     var myData = {list: self.displayList,user: self.user}
                    	console.log("The end of sign in... " + user);
-                    return user;
+                    return self.user;
                 });
             }).catch(function (error) {
                 $log.log("Authentication failed:", error);
