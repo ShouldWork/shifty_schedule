b7050ccc3f510e4931f4d1a9b6d169a938e034b9
ShouldWork <mrblocke@gmail.com>
2016-08-29 17:41:00 -0600

    update

--- src/js/apps.js
+++ src/js/apps.js
@@ -8,9 +8,13 @@
                 "ngMaterial",
                 "firebase"
             ])
-        .config(function($urlRouterProvider){
+        .config(function($urlRouterProvider,$stateProvider){
             $urlRouterProvider.otherwise(function($injector){
                 var $state = $injector.get('$state');
+            })
+            $stateProvider.state('home', {
+                url: '/home',
+                templateURL: 'src/html/groups.component.html'
             })
         })
 }());
