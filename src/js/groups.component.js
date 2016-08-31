1fe8393e28eab86b25b6c38ccf3d1dd9656364f0
ShouldWork <mrblocke@gmail.com>
2016-08-31 13:07:46 -0600

    update

--- src/js/groups.component.js
+++ src/js/groups.component.js
@@ -23,7 +23,7 @@
         }, true);
 
         function getTime(){
-            var date = new Date(timestamp * 1000);
+            var date = new Date();
             var dateObject = date.getFullYear() +'/'+ ('0' + (date.getMonth() + 1)).slice(-2) +'/'+ ('0' + date.getDate()).slice(-2);
             return dateObject;
         }      
