f57273fd6ea0af909e0ec62be3ce8511771e2182
ShouldWork <mrblocke@gmail.com>
2016-08-31 08:17:41 -0600

    update

--- src/services/schedule.service.js
+++ src/services/schedule.service.js
@@ -117,61 +117,19 @@
             	displayName: false
             };
         };
-  function signIn() {
-      if (!firebase.auth().currentUser) {
-        // [START createprovider]
-        var provider = new firebase.auth.GoogleAuthProvider();
-        // [END createprovider]
-        // [START addscopes]
-        provider.addScope('https://www.googleapis.com/auth/plus.login');
-        // [END addscopes]
-        // [START signin]
-        firebase.auth().signInWithPopup(provider).then(function(result) {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-          var token = result.credential.accessToken;
-          // The signed-in user info.
-          var user = result.user;
-          // [START_EXCLUDE]
-          // [END_EXCLUDE]
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // [START_EXCLUDE]
-          if (errorCode === 'auth/account-exists-with-different-credential') {
-            alert('You have already signed up with a different auth provider for that email.');
-            // If you are using multiple auth providers on your app you should handle linking
-            // the user's accounts here.
-          } else {
-            console.error(error);
-          }
-          // [END_EXCLUDE]
-        });
-        // [END signin]
-      } else {
-        // [START signout]
-        firebase.auth().signOut();
-        // [END signout]
-      }
-      // [START_EXCLUDE]
-      //document.getElementById('quickstart-sign-in').disabled = true;
-      // [END_EXCLUDE]
-    }
-        function signInxx(provider,msg) {
+
+        function signIn(provider,msg) {
             var auth = $firebaseAuth();
             return auth.$signInWithPopup(provider)
             	.then(loginSuccess)
             	.then(function(){
+                console.log("Second then")
 	            	self.isLoggedIn = true;
-	                self.userLoggedIn  = providerUser.displayName;
-	                $localStorage.user = {
+	              self.userLoggedIn  = providerUser.displayName;
+	              $localStorage.user = {
 	                	displayName: self.userLoggedIn,
 	                	isLoggedIn: self.isLoggedIn
-	                }
+	                };
             	})
             	.catch(loginError);
         }
