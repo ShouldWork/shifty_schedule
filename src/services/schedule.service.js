bc4437eb975fdabf47e85fe4cdbf7e2d1d89e5a0
ShouldWork <mrblocke@gmail.com>
2016-08-31 00:42:18 -0600

    Update schedule.service.js

--- src/services/schedule.service.js
+++ src/services/schedule.service.js
@@ -87,13 +87,6 @@
 
    	function logout(msg) {
             var auth = $firebaseAuth();
-     //       var ref = new Firebase("https//shifty-4d692+.firebaseio.com")
-     //       var authData = ref.getAuth();
-	    //if (authData) {
-	    //	console.log("Authenticated user with uid:", authData.uid);
-	    //} else {
-	    //	console.log("Not Authenticated user with uid:", authData.uid);
-	    //}
 	    var firebaseUser = self.firebUser;
             var message = self.getToastMsg(msg);
             var providerUser = firebaseUser.user ? firebaseUser.user : firebaseUser;
@@ -124,8 +117,51 @@
             	displayName: false
             };
         };
-
-        function signIn(provider,msg) {
+  function signIn() {
+      if (!firebase.auth().currentUser) {
+        // [START createprovider]
+        var provider = new firebase.auth.GoogleAuthProvider();
+        // [END createprovider]
+        // [START addscopes]
+        provider.addScope('https://www.googleapis.com/auth/plus.login');
+        // [END addscopes]
+        // [START signin]
+        firebase.auth().signInWithPopup(provider).then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          // [START_EXCLUDE]
+          // [END_EXCLUDE]
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/account-exists-with-different-credential') {
+            alert('You have already signed up with a different auth provider for that email.');
+            // If you are using multiple auth providers on your app you should handle linking
+            // the user's accounts here.
+          } else {
+            console.error(error);
+          }
+          // [END_EXCLUDE]
+        });
+        // [END signin]
+      } else {
+        // [START signout]
+        firebase.auth().signOut();
+        // [END signout]
+      }
+      // [START_EXCLUDE]
+      //document.getElementById('quickstart-sign-in').disabled = true;
+      // [END_EXCLUDE]
+    }
+        function signInxx(provider,msg) {
             var auth = $firebaseAuth();
             return auth.$signInWithPopup(provider)
             	.then(loginSuccess)
