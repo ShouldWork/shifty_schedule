a32548504ebef84015ac43261b9adab4b58645e5
ShouldWork <mrblocke@gmail.com>
2016-08-31 13:05:48 -0600

    update

--- src/js/groups.component.js
+++ src/js/groups.component.js
@@ -27,7 +27,7 @@
             var dateObject = date.getFullYear() +'/'+ ('0' + (date.getMonth() + 1)).slice(-2) +'/'+ ('0' + date.getDate()).slice(-2);
             return dateObject;
         }      
-    }
     var now = groups.getTime();
     console.log(now);
+    }
 })();
